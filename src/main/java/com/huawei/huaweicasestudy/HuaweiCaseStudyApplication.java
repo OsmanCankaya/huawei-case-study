0a909401a1e387bb61c00d07a98281aaaa399ef0
osman <osmancankayabilmuh@gmail.com>
2025-01-27 13:00:56 +0300

    Initial Commit

---
+++ src/main/java/com/huawei/huaweicasestudy/HuaweiCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.huawei.huaweicasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaweiCaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuaweiCaseStudyApplication.class, args);
+    }
+
+}
